9b32258fd2f23f29807ef80a9a83ad4efdaa11ff
Kirby Vaughn <64096766+kirbyVaughn@users.noreply.github.com>
2020-10-29 14:47:09 -0500

    upload files

---
+++ Aissign4/Assignment4P/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Assignment4PConfig(AppConfig):
+    name = 'Assignment4P'
---
+++ Aissign4/vue_env/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kirby\onedrive\desktop\school_work\isqa_4900\aissign4\vue_env\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
