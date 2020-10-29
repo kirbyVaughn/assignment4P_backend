aa84f8115ce3ab1f4059396dda041d4ad96355e6
Kirby Vaughn <64096766+kirbyVaughn@users.noreply.github.com>
2020-10-29 15:05:15 -0500

    upload files

--- Aissign4/Assignment4P/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Assignment4PConfig(AppConfig):
-    name = 'Assignment4P'
--- vue_env/myvenv/Scripts/activate.bat
+++ vue_env/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kirby\OneDrive\Desktop\School_work\ISQA_4900\Aissign4\vue_env\myvenv
+set VIRTUAL_ENV=C:\Users\kirby\OneDrive\Desktop\ISQA4900_repos\front_end_SP1\Front_End_vue\assignment4P_backend\vue_env\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
